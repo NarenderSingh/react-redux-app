690fd8868b98189f2a6820ec1e93cb623d2350f7
NarenderSingh <naren.delhi13@gmail.com>
2020-03-22 17:33:09 +0530

    Action payload. Passing value in reducers

---
+++ src/components/NewCakeContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { buyCake } from "../redux/index";
+
+function NewCakeContainer(props) {
+  const [number, setNumber] = useState(1);
+  return (
+    <div>
+      <h2>Number of Cakes : {props.numberOfCakes}</h2>
+      <input
+        type="text"
+        value={number}
+        onChange={e => setNumber(e.target.value)}
+      ></input>
+      <button
+        className="btn btn-primary btn-sm"
+        onClick={() => props.buyCake(number)}
+      >
+        Buy {number} Cake
+      </button>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    numberOfCakes: state.cake.numberOfCakes
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: number => dispatch(buyCake(number))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer);
